17cd0318a48dce58cc5db53882e45d89ea5620d8
Jason Madar <env3d@users.noreply.github.com>
2025-03-13 05:22:58 +0000

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
-# # conftest.py
-# import pytest
-# import os
+# conftest.py
+import pytest
+import os
 
-# def pytest_sessionstart(session):
-#     os.chdir(os.path.abspath('/home/runner/work/security-jwt-server/security-jwt-server'))
+def pytest_sessionstart(session):
+    os.chdir(os.path.abspath('/github.workspace'))
